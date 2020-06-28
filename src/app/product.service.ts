c1f65fc9b76ad96094bb81f027357022f08d4c07
rajal Vyas <rajal-v.emcins.com>
2020-06-27 19:34:30 -0500

    Project

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,15 @@ import {Http, Response } from '@angular/http';
 import { AlbumStorePage } from '../../e2e/app.po';
 import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operator/map';
+import {Album} from './album';
+import {Observable}  from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
    private _albumUrl = '../assets/album.json';
 
    constructor(private _http:Http) { }
-   getAlbum (id: number) {
+   Observable<Album> getAlbum (id: number) {
      return this._http.get(this._albumUrl).map((response) => response.json());
    }
 }
